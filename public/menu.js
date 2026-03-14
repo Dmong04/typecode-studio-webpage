7a8e70779a0cafec1ab96683ea710b1f68ca48f1
Dmong04 <168316345+Dmong04@users.noreply.github.com>
2026-03-13 22:08:52 -0600

    Cambios para el laboratorio

--- public/menu.js
+++ public/menu.js
@@ -1,13 +1,13 @@
-const menu = document.getElementById('menu');
-const closeMenu = document.getElementById('close-menu');
+const menu = document.getElementById('mobile-menu');
 const openMenu = document.getElementById('open-menu');
-
-closeMenu.addEventListener('click', () => {
-    menu.classList.remove('max-md:w-full');
-    menu.classList.add('max-md:w-0');
-});
+const closeMenu = document.getElementById('close-menu');
 
 openMenu.addEventListener('click', () => {
-    menu.classList.remove('max-md:w-0');
-    menu.classList.add('max-md:w-full');
+    menu.classList.remove('-translate-x-full');
+    menu.classList.add('translate-x-0');
+});
+
+closeMenu.addEventListener('click', () => {
+    menu.classList.remove('translate-x-0');
+    menu.classList.add('-translate-x-full');
 });
